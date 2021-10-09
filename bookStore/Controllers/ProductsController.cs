c1f33eb9630e7bc889ab2bedf82673af6e24dff0
Tomasz Topor <tomasz.topor@microsoft.wsei.edu.pl>
2021-10-09 16:27:28 +0200

    first

---
+++ bookStore/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using bookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add(ProductModel product)
+        {
+            var viewModel = new ProductsStatsViewModel
+            {
+                NameLength = product.Name.Length,
+                DescriptionLength = product.Description.Length,
+
+
+            };
+        }
+    }
+}
---
+++ bookStore/Models/ProductsStatsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookStore.Models
+{
+    public class ProductsStatsViewModel
+    {
+        public int NameLength { get; set; }
+        public int DescriptionLength { get; set; }
+    }
+}
